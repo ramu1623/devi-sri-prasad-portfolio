fb880c6138a0f15308fb6f52e561d5784fe7724e
d devi sri prasad <ramudsp23@gmail.com>
2026-05-16 20:31:34 +0530

    Dynamic port number

--- Backend/server.js
+++ Backend/server.js
@@ -26,7 +26,8 @@ app.get("/", (req, res) => {
 });
 
 // server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
